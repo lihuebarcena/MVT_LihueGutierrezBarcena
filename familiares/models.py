d90d8070e2fe5a34779260bb4581a9b5d73e0d7d
lihuebarcena <lihuebarcena@gmail.com>
2022-08-31 21:19:46 -0300

    Cambié nombre del diccionario y el nombre del iterador

--- familiares/models.py
+++ familiares/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class familiares(models.Model):
+class familiares_lista(models.Model):
     parentesco=models.CharField(max_length=100)
     name=models.CharField(max_length=100)
     edad=models.IntegerField()
--- familiares/views.py
+++ familiares/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Template, Context, loader
-from familiares.models import familiares
+from familiares.models import familiares_lista
 
 def listar_familiares(request):
-    queryset=familiares.objects.all()
+    queryset=familiares_lista.objects.all()
     diccio={'familiares':queryset}
     plantilla=loader.get_template('familiares_list.html')
     documento_html=plantilla.render(diccio)
