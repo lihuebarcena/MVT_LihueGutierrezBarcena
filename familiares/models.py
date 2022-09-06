51d13115473f761f2ebc7f355c6ef4ffcf0388d9
lihuebarcena <lihuebarcena@gmail.com>
2022-09-06 19:22:36 -0300

    Modifaciones nuevas de Anderson: modifiqué models, views

--- familiares/models.py
+++ familiares/models.py
@@ -3,9 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-class familiares_lista(models.Model):
+class familiares(models.Model):
     parentesco=models.CharField(max_length=100)
     name=models.CharField(max_length=100)
     edad=models.IntegerField()
     year_born=models.DateField()
     
+    def __str__(self):
+        return f'{self.name} || {self.edad}'
--- familiares/views.py
+++ familiares/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Template, Context, loader
-from familiares.models import familiares_lista
+from familiares.models import familiares
 
 def listar_familiares(request):
-    queryset=familiares_lista.objects.all()
-    diccio={'familiares':queryset}
+    #Yo cargué los datos por consola por eso deberían estar cargados, no funciona así?
+    # queryset=familiares.objects.all()
+    #diccio={'familiares':queryset}
+    user=familiares('primo','Pedro',22,'2000-10-10')
+    user.save()
+
+    diccio={'familiares':[familiares for familiares in familiares.objects.values()]}
+
     plantilla=loader.get_template('familiares_list.html')
     documento_html=plantilla.render(diccio)
+    
     return HttpResponse(documento_html)
