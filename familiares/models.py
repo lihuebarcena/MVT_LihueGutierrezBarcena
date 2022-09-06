dc2d2c18fbacc0e750c97c02c7e6a5fe163a6f8e
lihuebarcena <lihuebarcena@gmail.com>
2022-09-06 20:18:17 -0300

    Modifiqué template

--- familiares/models.py
+++ familiares/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class familiares(models.Model):
+class Familiar(models.Model):
+    id=models.IntegerField(primary_key=True,unique=True)
     parentesco=models.CharField(max_length=100)
     name=models.CharField(max_length=100)
     edad=models.IntegerField()
--- familiares/views.py
+++ familiares/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Template, Context, loader
-from familiares.models import familiares
+from familiares.models import Familiar
 
 def listar_familiares(request):
     #Yo cargué los datos por consola por eso deberían estar cargados, no funciona así?
-    # queryset=familiares.objects.all()
-    #diccio={'familiares':queryset}
-    user=familiares('primo','Pedro',22,'2000-10-10')
+    #queryset=Familiar.objects.all()
+    #diccio={'Familiar':queryset}
+    user=Familiar(1,'primo','Pedro',22,'2000-10-10')
     user.save()
 
-    diccio={'familiares':[familiares for familiares in familiares.objects.values()]}
+    diccio={'familiares':[familiares for familiares in Familiar.objects.values()]}
 
     plantilla=loader.get_template('familiares_list.html')
     documento_html=plantilla.render(diccio)
